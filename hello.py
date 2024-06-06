d58dde1687e629b4fd9e0a72a3330864fe2e7456
shakscode19 <sakinyemiceo@gmail.com>
2024-06-06 04:43:20 -0500

    I created a file in gitIDE envt

---
+++ hello.py
@@ -0,0 +1,4 @@
+print("I am a senior python developer solving real world problems"
+print("I will land my first junor developer role for 100k"
+print("My name is Shakcode19"
+print("I have no excuse to fail" 
